c66a61cbbc4723d34b722705ba077d419f0cffb3
aimurat-dilmuhamed <najzabekovaajgul@gmail.com>
2026-04-14 18:18:14 +0500

    Add Practice9 - Pygame games: Mickey's clock, music player, moving ball

--- lab9/mickeys_clock/clock.py
+++ lab9/mickeys_clock/clock.py
@@ -7,35 +7,38 @@ class MickeyClock:
         self.screen_size = (screen_width, screen_height)
         self.center = (screen_width // 2, screen_height // 2)
         
-        # Load Assets
-        self.bg = pygame.image.load("clock.jpg")
+        # 1. Load the background clock face
+        self.bg = pygame.image.load("images/clock.jpg")
         self.bg = pygame.transform.scale(self.bg, self.screen_size)
         
-        # Minutes Hand (Right hand)
-        self.min_hand_orig = pygame.image.load("hand_right_centered.png").convert_alpha()
-        # Seconds Hand (Left hand)
-        self.sec_hand_orig = pygame.image.load("hand_left_centered.png").convert_alpha()
+        # 2. Load Mickey's body (The missing piece!)
+        self.mickey_body = pygame.image.load("images/mikkey.png").convert_alpha()
+        # Scale him to fit nicely in the middle
+        self.mickey_body = pygame.transform.scale(self.mickey_body, (400, 600)) 
+        self.mickey_rect = self.mickey_body.get_rect(center=self.center)
+        
+        # 3. Load Hands
+        self.min_hand_orig = pygame.image.load("images/hand_right_centered.png").convert_alpha()
+        self.sec_hand_orig = pygame.image.load("images/hand_left_centered.png").convert_alpha()
 
     def render(self, surface):
-        # 1. Draw Background
+        # Draw Background first
         surface.blit(self.bg, (0, 0))
         
-        # 2. Get Time
+        # Draw Mickey's body next (behind the hands)
+        surface.blit(self.mickey_body, self.mickey_rect.topleft)
+        
+        # Get Time and Rotate Hands
         now = datetime.datetime.now()
-        # We subtract from 0 or use negative because Pygame rotates CCW
-        # 360 degrees / 60 units = 6 degrees per unit
         min_angle = -(now.minute * 6)
         sec_angle = -(now.second * 6)
 
-        # 3. Rotate Hands
-        # Rotate the minute hand
         rotated_min = pygame.transform.rotate(self.min_hand_orig, min_angle)
         min_rect = rotated_min.get_rect(center=self.center)
         
-        # Rotate the second hand
         rotated_sec = pygame.transform.rotate(self.sec_hand_orig, sec_angle)
         sec_rect = rotated_sec.get_rect(center=self.center)
 
-        # 4. Blit to surface
+        # Draw hands on top of everything
         surface.blit(rotated_min, min_rect.topleft)
         surface.blit(rotated_sec, sec_rect.topleft)
